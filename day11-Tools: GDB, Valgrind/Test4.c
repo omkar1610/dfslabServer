95080cb0ba2f3ab795c84209e232f4562d437bf6
omkar1610 <omkar.ashrit@gmail.com>
2019-08-31 22:26:08 +0530

    2019-08-31

--- day11-Tools: GDB, Valgrind/Test4.c
+++ day11-Tools: GDB, Valgrind/Test4.c
@@ -8,11 +8,13 @@ int main(){
 
 	// Allocation no. 1 of 20 bytes
 	ptr = (char *) malloc(20);
+	free(ptr);
 	// Allocation no. 2 of 10 bytes
 	ptr = (char *) malloc(10);
 	free(ptr);
-	free(ptr);
+	//free(ptr);
 	// Allocation no. 3 of 30 bytes
 	ptr = (char *) malloc(16);
+	free(ptr);
 	return 0;
 }
