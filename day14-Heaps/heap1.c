8533159143619d45eaa5f7cb230fce6a1bf54736
Omkar Ashrit <omkar.ashrit@gmail.com>
2019-09-17 23:14:34 +0530

    2019-09-17

--- day14-Heaps/heap1.c
+++ day14-Heaps/heap1.c
@@ -48,8 +48,62 @@ void showArrFloat(HEAP *h)
 
 void prog1(HEAP *h)
 {
-	
+	float m1, m2, m3;
 
+	float *arr = malloc(sizeof(int) * h->num_used); int i;
+	deleteMax(h, &m1);
+	deleteMax(h, &m2);
+	deleteMax(h, &m3);
+	if(m1 == m2)
+	{		
+		if(m2 == m3)
+		{
+			printf("h1\n");
+			//Array of {m1}
+			i = 0;
+			arr[i++] = m1;
+			arr[i++] = m2;
+			arr[i++] = m3;
+		}
+		else if(m2 != m3)
+		{
+			printf("h2\n");
+			//Array of {m1, m2}  {m3 array}
+			i = 0;
+			arr[i++] = m3;
+		}
+	}
+	else if(m1 != m2)
+	{
+		if(m2 == m3)
+		{
+			printf("h3\n");
+			//Array of {m1}  {m2, m3 array}
+			i = 0;
+			arr[i++] = m2;
+			arr[i++] = m3;
+		}
+		else if(m2 != m3)
+		{
+			printf("h4\n");
+			//Array of {m1} {m2}  {m3 array}
+			i = 0;
+			arr[i++] = m3;
+		}
+	}
+	//keep on getting till you get same as m3
+	showArrFloat(h);
+
+	float tmp = m3;
+	while(tmp == m3)
+	{
+		deleteMax(h, &tmp);
+		if(tmp == m3)
+			arr[i++] = tmp;
+	}
+	insert(h, &tmp);
+	for(int J = 0; J<i;J++)
+		printf("%0.2f",arr[J] );printf("\n");
 
 }
 
@@ -57,7 +111,7 @@ void prog1(HEAP *h)
 int main()
 {
 	HEAP heap; HEAP *h = &heap;
-	initHeap(h, sizeof(NODE),  compare_float);
+	initHeap(h, sizeof(float),  compare_float);
 	
 	float x = 0.5; insert(h, &x);
 	x = 0.3; insert(h, &x);
@@ -65,8 +119,10 @@ int main()
 	x = 0.1; insert(h, &x);
 	x = 0.8; insert(h, &x);
 	x = 0.2; insert(h, &x);
+	x = 0.3; insert(h, &x);
 	showArrFloat(h);
 	prog1(h);
+	showArrFloat(h);
 
 	return 0;
 }
