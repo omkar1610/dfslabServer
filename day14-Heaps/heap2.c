8970edc934fa3d10effa4a3a596130ae6ce81bd8
omkar1610 <omkar.ashrit@gmail.com>
2019-09-20 03:59:30 +0530

    2019-09-20

--- day14-Heaps/heap2.c
+++ day14-Heaps/heap2.c
@@ -1,35 +1,9 @@
 #include "heap.h"
 
-/*
-typedef struct
-{
-	size_t element_size; // Generic implementation 
-	unsigned int num_allocated, num_used; // Keep track of the size
-	void *array; // Using one-based indexing
-	int (*comparator)(void *, int, int); // Returns -ve, 0, or +ve
-}HEAP;
-
-void initHeap(HEAP *h, size_t element_size,int (*comparator)(void *, int, int))
-
-void swap(HEAP *h, int i, int j)
-
-static void swapUp(HEAP *h, int k)
-
-void insert(HEAP *h, void *x)
-
-static void swapDown(HEAP *h, int k)
-
-void deleteMax(HEAP *h, void *max)
-
-static int compare_int(void *array, int i1, int i2)
-*/
-
-
-
 void prog2(HEAP *h)
 {
 	//Take Input
-	int k = 3, j;
+	int k = 3, j, total_ele;
 	char ***ptr = malloc(k*sizeof(char **));//Array of lists containg 
 											//list of strings
 	for(int i = 0; i<k; i++)
@@ -40,6 +14,8 @@ void prog2(HEAP *h)
 	
 		char *tmp = strtok(ptr[i][j], " ");
 		j++;
+		total_ele += atoi(tmp);
+		
 		while(NULL != (tmp = strtok(NULL, " ")))
 		{
 			ptr[i]  = realloc(ptr[i], (j+1)*sizeof(char *)); 
@@ -52,7 +28,7 @@ void prog2(HEAP *h)
 
 	node2d n2d;
 	
-	int *result = malloc(50*sizeof(int));int r_loc = 0;
+	int *result = malloc((1+total_ele)*sizeof(int));int r_loc = 0;
 
 	for(int i = 0; i<k ; i++)
 		{
