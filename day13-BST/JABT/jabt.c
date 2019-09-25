b93f6486d4a7e3cc98381f183fc50e7df2802376
omkar1610 <omkar.ashrit@gmail.com>
2019-09-25 22:55:06 +0530

    2019-09-25

--- day13-BST/JABT/jabt.c
+++ day13-BST/JABT/jabt.c
@@ -10,52 +10,7 @@ void dataPrint(void *data)
 	printf("%d ",*(int*)(data) );
 }
 
-void postorder_iterative_single_stack(BTREE *btree)// Not DOne Properly
-{
-	// Base Case
-      
-    STACK s; 
-	initStack(&s, sizeof(int));
-	int curr = 0; 
-	do
-    { 
-        // Move to leftmost node 
-        while (curr != -1) 
-        { 
-            // Push root's right child and then root to stack. 
-           
-            if (CURR_RIGHT != -1)
-    		{ 
-	            int tmp = CURR_RIGHT;
-	            push(&s, &tmp); 
-	        }
-            push(&s, &curr); 
-  
-            // Set root as root's left child   
-            curr = CURR_LEFT; 
-        } 
-  
-        // Pop an item from stack and set it as root     
-        pop(&s, &curr);
-  
-        // If the popped item has a right child and the right child is not 
-        // processed yet, then make sure right child is processed before root 
-        int tmp; peek(&s, &tmp);
-        if (CURR_RIGHT != -1 && tmp == CURR_RIGHT) 
-        { 
-                        
-            pop(&s, &tmp);  
-            push(&s, &curr); // push root back to stack 
-            curr = CURR_RIGHT; // change root so that the right  
-                                // child is processed next 
-        } 
-        else  // Else print root's data and set root as NULL 
-        { 
-            btree->dataPrint(btree->array[curr]->data);
-            curr = -1;
-        } 
-    } while (s.num_elements != 0);
-}
+
 
 int main(int argc, char const *argv[])
 {
--- headers/Binary Tree/justBinaryTree.c
+++ headers/Binary Tree/justBinaryTree.c
@@ -186,3 +186,49 @@ void postorder_iterative(BTREE *btree) //Using 2 stacks
 }
 
 
+void postorder_iterative_single_stack(BTREE *btree)// Not DOne Properly
+{
+    // Base Case
+      
+    STACK s; 
+    initStack(&s, sizeof(int));
+    int curr = 0; 
+    do
+    { 
+        // Move to leftmost node 
+        while (curr != -1) 
+        { 
+            // Push root's right child and then root to stack. 
+           
+            if (CURR_RIGHT != -1)
+            { 
+                int tmp = CURR_RIGHT;
+                push(&s, &tmp); 
+            }
+            push(&s, &curr); 
+  
+            // Set root as root's left child   
+            curr = CURR_LEFT; 
+        } 
+  
+        // Pop an item from stack and set it as root     
+        pop(&s, &curr);
+  
+        // If the popped item has a right child and the right child is not 
+        // processed yet, then make sure right child is processed before root 
+        int tmp; peek(&s, &tmp);
+        if (CURR_RIGHT != -1 && tmp == CURR_RIGHT) 
+        { 
+                        
+            pop(&s, &tmp);  
+            push(&s, &curr); // push root back to stack 
+            curr = CURR_RIGHT; // change root so that the right  
+                                // child is processed next 
+        } 
+        else  // Else print root's data and set root as NULL 
+        { 
+            btree->dataPrint(btree->array[curr]->data);
+            curr = -1;
+        } 
+    } while (s.num_elements != 0);
+}
