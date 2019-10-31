0c3f974099b5c2b892a3404e510a719801ab7f06
Omkar Ashrit <omkar.ashrit@gmail.com>
2019-10-31 13:58:02 +0530

    2019-10-31

--- Assign/assign3/1.c
+++ Assign/assign3/1.c
@@ -6,44 +6,89 @@
     char *tmp = a[j]; \
     a[j] = a[i]; \
     a[i] = tmp; \
+    count++; \
 }
+
+#define DATA {\
+    for (int k = 1; k < argc; ++k) \
+        printf("%s ", a[k]); \
+    printf("\n"); \
+}
+
+#define CMP(i,j) strcmp(a[(i)],a[(j)])
 
 int main(int argc, char *a[])
 {
-    for (int i = 1; i < argc; ++i)
-        printf("%s ", a[i]);
-    printf("\n");
+    int count = 0;
+    // for (int i = 1; i < argc; ++i)        printf("%d ", i);    printf("\n");
 
+    int n = argc -1;
+    // DATA;
+    // a[1], a[2], .... , a[argc-2], a[argc-1]
+    // printf("FORWARD\n");
     for (int i = 1; i < argc-1; ++i)
     {
-        if(strcmp(a[i],a[i+1]) == 0)
+        if(CMP(i, i+1) == 0)
         {
             int j = i+2;
-            while(j<argc && strcmp(a[i],a[j])==0)
+            int flag = 1;
+            // Find appropriate j to swap with i+1  in forward pass
+            while(j<=argc-1 && flag)
+            {
+                // printf("\ni j %d %d\n", i, j);
+                // i neq j
+                if(CMP(i,j) != 0 /*&& printf("j = %d ", j)*/)
+                {
+                    flag = 0;
+                    // i neq j-1
+                    if(j>i+2 && CMP(i,j-1) == 0 /*&& printf("j-1 %d ", j-1)*/) flag = 1;
+                    // i neq j+1
+                    if(j<argc-1 && CMP(i, j+1) == 0 /*&& printf("j+1 %d\n", j+1)*/) flag = 1;
+                    // Swap if all 3 above is satisfied
+                    if(flag == 0) SWAP(i+1, j);
+                    // printf("\nswapping %d %d\n", i, j);
+    
+                    // DATA;
+
+                }
                 j++;
-            if(j<argc)
-            {
-                SWAP(j, i+1);
             }
-            else
-            {
-                j = i-2;
-                while(j>=0 && strcmp(a[i], a[j]) == 0 && (j<1 || strcmp(a[j], a[j-1]) != 1))
-                    j--;
-                if(j>=0)
-                    SWAP(j, i+1);
-            }
-
-            for (int k = 1; k < argc; ++k)
-        printf("%s ", a[k]);
-    printf("\n");
+            
         }
-        
 
+        
+    }
+    // DATA;
+    // printf("BACKWARD\n");
+    for (int i = argc-1; i >1 ; --i)
+    {
+        if(CMP(i, i-1) == 0)
+        {
+            int j = i-2;
+            int flag = 1;
+            while(j>=1 && flag)
+            {
+                // printf("\ni j %d %d\n", i, j);
+                // i neq j
+                if(CMP(i,j) != 0 /*&& printf("j = %d ", j)*/)
+                {
+                    flag = 0;
+                    // i neq j-1
+                    if(j<i+2 && CMP(i,j-1) == 0 /*&& printf("j-1 %d ", j-1)*/) flag = 1;
+                    // i neq j+1
+                    if(j>1 && CMP(i, j+1) == 0 /*&& printf("j+1 %d\n", j+1)*/) flag = 1;
+                    // Swap if all 3 above is satisfied
+                    if(flag == 0) SWAP(i-1, j);
+                    // printf("\nswapping %d %d\n", i, j);
+                    // DATA;
+
+                }
+                j--;
+            }
+        }
     }
 
-    for (int i = 1; i < argc; ++i)
-        printf("%s ", a[i]);
-    printf("\n");
+    printf("No of swaps : %d\n", count);
+    DATA;
     return 0;
 }
