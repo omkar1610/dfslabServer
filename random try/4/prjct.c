b5f141cca36ba1557ca70498c8ea76fa577cd851
Omkar Ashrit <30653006+omkar1610@users.noreply.github.com>
2019-12-10 22:10:13 +0530

    Update prjct.c

--- random try/4/prjct.c
+++ random try/4/prjct.c
@@ -3,14 +3,20 @@
 #include <string.h>
 #include <math.h>
 
-#define n 200
+#define n 2000
 #define C_l 50
-#define numActive B[0]
-#define deltaLast B[1]
-#define w sizeof(int)
-#define num_LL (int) n/C_l
-#define ele_L (int)floor(log2(n))
 
+#define numActive *(int*)B
+#define deltaLast *((int*)B + 1)
+
+#define CELL_SIZE_B sizeof(int)
+#define CELL_SIZE_L (int)ceil(log2(n))
+
+#define CELL_COUNT_B n
+#define CELL_COUNT_L (int) n/C_l
+
+#define CELL_of_B_in_L (int)ceil(((float)CELL_COUNT_L * CELL_SIZE_L)/(8 * CELL_SIZE_B))
+#define N_base (2 + CELL_of_B_in_L)
 
 struct
 {
@@ -18,16 +24,36 @@ struct
 }Node;
 
 
+struct {
+   unsigned int age : CELL_SIZE_L;
+} Age;
+
 int main()
 {
-	int* B = malloc(n * sizeof(int));
-	printf("each bit of L = %d\n", (int)floor(log2(n)));
+	void* B = malloc(CELL_COUNT_B * CELL_SIZE_B);
 
-	printf("size of B = %lu bytes(%lu bits)\n", n * sizeof(int), 8 * n * sizeof(int));
-	printf("size of G = %lu bytes(%lu bits)\n", 2 * sizeof(int), 8 * 2 * sizeof(int));
-	printf("size of L = ? bytes(%d bits)\n", num_LL * ele_L);
-	printf("cells of B in  L = %d\n", (int)ceil((num_LL * ele_L)/(8 * sizeof(int))));
+	// printf("B = %d cells = %lu bytes(%lu bits)\n", CELL_COUNT_B, CELL_COUNT_B * CELL_SIZE_B, 8 * CELL_COUNT_B * CELL_SIZE_B);
 
+	// printf("G = %d cells = %lu bytes(%lu bits)\n", 2, 2 * CELL_SIZE_B, 8 * 2 * CELL_SIZE_B);
+	
+	// printf("L = %d cells = %lu bytes(%lu bits)\n", CELL_COUNT_L, CELL_COUNT_L * CELL_SIZE_L, 8 * CELL_COUNT_L * CELL_SIZE_L);
+
+	// printf("L = %d cell of B", CELL_of_B_in_L);
+
+	printf("CELL_SIZE_B %luB \nCELL_SIZE_L %db \nCELL CELL_COUNT_L %d \nCELL_of_B_in_L %d\n", CELL_SIZE_B , CELL_SIZE_L , CELL_COUNT_L , CELL_of_B_in_L);
+	printf("N_base %d\n", N_base);
+
+	numActive = 0;
+	deltaLast = 2;
+
+
+	Age.age = 20;
+	printf( "Sizeof( Age ) : %d\n", sizeof(Age) );
+	printf( "Age.age : %d\n", Age.age );
+
+	unsigned int j : 3;
+	j = 5;
+	printf("%d\n", j);
 
 
 
