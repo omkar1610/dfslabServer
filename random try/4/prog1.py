2f122ebd117464e64e4bcfd4d19aa4211cb1f108
omkar1610 <omkar.ashrit@gmail.com>
2019-12-11 19:46:32 +0530

    2019-12-11

--- random try/4/prog1.py
+++ random try/4/prog1.py
@@ -1,26 +1,6 @@
 import sys
 import re
 
-def func_old(ip, op):
-	with open(ip) as f:
-	    data = list(f.read())
-	f.close()
-
-	delims = ["\t",",",".",";",":","|","\'","\"","{","}","\\","/","`"," ","-"]
-	freq = [data.count(c) for c in delims]
-	max_freq = max(freq)
-	if freq.count(max_freq) > 1:
-		print("CONFUSING")
-		sys.exit(1)
-	new_delim = delims[freq.index(max_freq)]
-
-	out_data = [new_delim if ele in delims else ele for ele in data]
-
-	with open(op,"w") as f:
-	    f.write("".join(out_data))
-	f.close()
-	print("".join(out_data))
-
 
 def func(ip, op):
 	with open(ip) as f:
